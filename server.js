1f93ce22c39d7adf4cea9ddd2bfc283813a6697b
nahidankur <nahidkarim08@gmail.com>
2023-06-24 00:09:15 +0600

    setup mongodb connection

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db')
 
 const app = express();
-
+connectDB()
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000
 
