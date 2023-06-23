004edb20815fa224a5a05a804d0ea711fa94ffe4
nahidankur <nahidkarim08@gmail.com>
2023-06-24 00:16:07 +0600

    added user model

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async ()=> {
     try {
@@ -6,9 +6,9 @@ const connectDB = async ()=> {
         console.log('Mongodb Connected Successfully')
         
     } catch (error) {
-        console.log(error)
-        process.exit(1)
+        console.log(error);
+        process.exit(1);
     }
 }
 
-module.exports = connectDB
+module.exports = connectDB;
