131ae0f8a3b5c602718d3f0d41bce525ac6a9bd0
nahidankur <nahidkarim08@gmail.com>
2023-06-24 14:02:31 +0600

    added authentication routes and controllers

---
+++ controllers/authController.js
@@ -0,0 +1,60 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+
+const register = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    // Create a new user
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+
+    // Generate JWT
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.json({ username: user.email, token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Checking if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+    // Comparing passwords
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+    // Generate JWT
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.json({ username: user.email, token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+module.exports = { register, login };
--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db')
+const authRoutes =  require('./routes/authRoutes')
 
 const app = express();
 connectDB()
 app.use(express.json());
 app.use(cookieParser());
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000
 
