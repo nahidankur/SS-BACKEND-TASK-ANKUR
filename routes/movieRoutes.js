0b58007a3338287cf5f1f301fd290701e0b513ad
Nahid Karim Ankur <nahidankur@gmail.com>
2023-06-26 23:52:48 +0600

    added update and delete routes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createMovie, getAllMovies, getMovieById} = require('../controllers/movieController');
+const {createMovie, getAllMovies, getMovieById, updateMovie, deleteMovie} = require('../controllers/movieController');
 const authMiddleware =  require('../middleware/authMiddleware');
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get('/', getAllMovies )
 router.get('/:id', getMovieById )
 router.post('/', authMiddleware, createMovie);
+router.put('/:id', authMiddleware, updateMovie);
+router.delete('/:id', authMiddleware, deleteMovie);
 
 module.exports = router;
